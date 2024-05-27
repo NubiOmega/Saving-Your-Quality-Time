63d6dd00b98513c439578414fc3fa1e85d7144a9
Davians <conelomultimedia@gmail.com>
2024-05-28 03:05:17 +0700

    ptimalisasi kode, error message, format waktu optimalisasi kode, error message, format waktu
    Validasi Nilai Input:
    Fungsi simpan_pengaturan_conf sekarang memeriksa apakah nilai suhu dan waktu holding time valid. Jika tidak valid, maka akan muncul pesan peringatan.
    Penanganan Error:
    Menggunakan QMessageBox untuk menampilkan pesan error jika terjadi kesalahan dalam proses penyimpanan atau pembacaan konfigurasi.
    Fallback Default Values:
    Jika file konfigurasi tidak mengandung nilai yang diharapkan, fungsi baca_pengaturan_conf menyediakan nilai default sehingga aplikasi tetap dapat berjalan
    
    Penanganan Error:
    Setiap fungsi sekarang memiliki penanganan error menggunakan try dan except untuk menampilkan pesan error yang tepat jika terjadi kesalahan.
    Validasi Input:
    Fungsi validate_and_start_import memeriksa apakah ada file yang diunggah sebelum memulai proses impor, dan jika tidak, menampilkan pesan peringatan.
    Pesan Error:
    Pesan error ditampilkan menggunakan QMessageBox untuk memastikan pengguna mengetahui kesalahan yang terjadi tanpa aplikasi mengalami force close.
    
    Validasi Format Waktu dalam format "Jam:Menit:SS" (HH:MM:SS).
    Misalnya, untuk 2 jam, mereka dapat memasukkan "02:00:00". Format ini lebih mudah dipahami dan diinput oleh pengguna.
    Pada fungsi simpan_pengaturan_conf, sebelum menyimpan holding_time, dilakukan validasi menggunakan regex untuk memastikan formatnya sesuai.
    Jika format tidak sesuai, akan muncul pesan peringatan.
    Pesan Error:
    Pesan error ditampilkan menggunakan QMessageBox untuk memudahkan pengguna memahami kesalahan yang terjadi tanpa menyebabkan aplikasi crash.

--- Utilities/pengaturan_func.py
+++ Utilities/pengaturan_func.py
@@ -1,6 +1,77 @@
 
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QColorDialog
+from openpyxl.styles import PatternFill
+from configparser import ConfigParser
 
+def simpan_pengaturan_conf(self):
+    # Ambil nilai dari QDoubleSpinBox
+    batas_suhu_1 = self.ui.batasSuhu1_DoubleSpinBox.value()
+    batas_suhu_2 = self.ui.batasRangeSuhu2_DoubleSpinBox.value()
+    
+    # Ambil warna suhu dari QLabel
+    warna_suhu1 = self.ui.pilihWarnaSuhu1_btn.styleSheet().split("background-color:")[1].strip().replace(";", "")
+    warna_suhu2 = self.ui.pilihWarnaSuhu2_btn.styleSheet().split("background-color:")[1].strip().replace(";", "")
+    # Ambil warna holding time dari QLabel
+    warna_holding_time = self.ui.pilihWarnaHoldingTime_btn.styleSheet().split("background-color:")[1].strip().replace(";", "")
+    
+    # Ambil nilai holding time dari QLineEdit
+    holding_time = self.ui.holdingTime_lineEdit.text()
+
+    # Simpan nilai ke file konfigurasi
+    config = ConfigParser()
+    config['Pengaturan Suhu'] = {
+        'Suhu1': batas_suhu_1,
+        'Suhu2': batas_suhu_2
+    }
+    config['Pengaturan Fill Warna'] = {
+        'FillSuhu1': warna_suhu1,
+        'FillSuhu2': warna_suhu2,
+        'FillHoldingTime': warna_holding_time
+    }
+    config['Pengaturan Holding Time'] = {
+        'HoldingTime': holding_time
+    }
+    with open('pengaturan_app.conf', 'w') as configfile:
+        config.write(configfile)
+    
+    # Tampilkan pesan konfirmasi
+    QtWidgets.QMessageBox.information(self, "Berhasil Disimpan", "Konfigurasi Pengaturan telah disimpan.")
+
+def baca_pengaturan_conf(self):
+    config = ConfigParser()
+    try:
+        config.read('pengaturan_app.conf')
+        if 'Pengaturan Suhu' in config:
+            batas_suhu_1 = config.getfloat('Pengaturan Suhu', 'Suhu1')
+            batas_suhu_2 = config.getfloat('Pengaturan Suhu', 'Suhu2')
+            self.ui.batasSuhu1_DoubleSpinBox.setValue(batas_suhu_1)
+            self.ui.batasRangeSuhu2_DoubleSpinBox.setValue(batas_suhu_2)
+        if 'Pengaturan Fill Warna' in config:
+            fill_suhu1 = config.get('Pengaturan Fill Warna', 'Fillsuhu1', fallback='#FFFFFF')
+            fill_suhu2 = config.get('Pengaturan Fill Warna', 'Fillsuhu2', fallback='#FFFFFF')
+            fill_holding_time = config.get('Pengaturan Fill Warna', 'FillHoldingTime', fallback='#FFFFFF')
+            # Tampilkan warna suhu 1 pada label
+            self.ui.pilihWarnaSuhu1_btn.setStyleSheet(f"background-color: {fill_suhu1};")
+            self.fill_suhu1 = PatternFill(start_color=fill_suhu1.lstrip('#'),
+                                                    end_color=fill_suhu1.lstrip('#'),
+                                                    fill_type="solid")
+            # Tampilkan warna suhu 2 pada label
+            self.ui.pilihWarnaSuhu2_btn.setStyleSheet(f"background-color: {fill_suhu2};")
+            self.fill_suhu2 = PatternFill(start_color=fill_suhu2.lstrip('#'),
+                                                    end_color=fill_suhu2.lstrip('#'),
+                                                    fill_type="solid")
+            # Tampilkan warna holding time pada label
+            self.ui.pilihWarnaHoldingTime_btn.setStyleSheet(f"background-color: {fill_holding_time};")
+            self.fill_holding_time = PatternFill(start_color=fill_holding_time.lstrip('#'),
+                                                    end_color=fill_holding_time.lstrip('#'),
+                                                    fill_type="solid")
+        if 'Pengaturan Holding Time' in config:
+            holding_time = config.get('Pengaturan Holding Time', 'HoldingTime', fallback='9:00')
+            self.ui.holdingTime_lineEdit.setText(holding_time)
+    except Exception as e:
+        print(f'Error membaca file pengaturan: {e}')
+        
 def get_batas_suhu_1(self):
     return self.ui.batasSuhu1_DoubleSpinBox.value()
 
--- main_app.py
+++ main_app.py
@@ -12,7 +12,6 @@ from App.salin_data_waktu_suhu import *
 from App.validasi_data_waktu import *
 from Utilities.dragndrop_files_func import *
 from Utilities.pengaturan_func import *
-from configparser import ConfigParser
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -64,74 +63,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.window_compare_xls_xlsx = CompareXlsXlsxWindow()
         self.window_compare_xls_xlsx.show()
 
-    def simpan_pengaturan_conf(self):
-        # Ambil nilai dari QDoubleSpinBox
-        batas_suhu_1 = self.ui.batasSuhu1_DoubleSpinBox.value()
-        batas_suhu_2 = self.ui.batasRangeSuhu2_DoubleSpinBox.value()
-        
-        # Ambil warna suhu dari QLabel
-        warna_suhu1 = self.ui.fillWarnaSuhu1_label.styleSheet().split("background-color:")[1].strip().replace(";", "")
-        warna_suhu2 = self.ui.fillWarnaSuhu2_label.styleSheet().split("background-color:")[1].strip().replace(";", "")
-        # Ambil warna holding time dari QLabel
-        warna_holding_time = self.ui.fillWarnaHoldingTime_label.styleSheet().split("background-color:")[1].strip().replace(";", "")
-        
-        # Ambil nilai holding time dari QLineEdit
-        holding_time = self.ui.holdingTime_lineEdit.text()
-
-        # Simpan nilai ke file konfigurasi
-        config = ConfigParser()
-        config['Pengaturan Suhu'] = {
-            'Suhu1': batas_suhu_1,
-            'Suhu2': batas_suhu_2
-        }
-        config['Pengaturan Fill Warna'] = {
-            'FillSuhu1': warna_suhu1,
-            'FillSuhu2': warna_suhu2,
-            'FillHoldingTime': warna_holding_time
-        }
-        config['Pengaturan Holding Time'] = {
-            'HoldingTime': holding_time
-        }
-        with open('pengaturan_app.conf', 'w') as configfile:
-            config.write(configfile)
-        
-        # Tampilkan pesan konfirmasi
-        QtWidgets.QMessageBox.information(self, "Berhasil Disimpan", "Konfigurasi Pengaturan telah disimpan.")
-
-    def baca_pengaturan_conf(self):
-        config = ConfigParser()
-        try:
-            config.read('pengaturan_app.conf')
-            if 'Pengaturan Suhu' in config:
-                batas_suhu_1 = config.getfloat('Pengaturan Suhu', 'Suhu1')
-                batas_suhu_2 = config.getfloat('Pengaturan Suhu', 'Suhu2')
-                self.ui.batasSuhu1_DoubleSpinBox.setValue(batas_suhu_1)
-                self.ui.batasRangeSuhu2_DoubleSpinBox.setValue(batas_suhu_2)
-            if 'Pengaturan Fill Warna' in config:
-                fill_suhu1 = config.get('Pengaturan Fill Warna', 'Fillsuhu1', fallback='#FFFFFF')
-                fill_suhu2 = config.get('Pengaturan Fill Warna', 'Fillsuhu2', fallback='#FFFFFF')
-                fill_holding_time = config.get('Pengaturan Fill Warna', 'FillHoldingTime', fallback='#FFFFFF')
-                # Tampilkan warna suhu 1 pada label
-                self.ui.pilihWarnaSuhu1_btn.setStyleSheet(f"background-color: {fill_suhu1};")
-                self.fill_suhu1 = PatternFill(start_color=fill_suhu1.lstrip('#'),
-                                                     end_color=fill_suhu1.lstrip('#'),
-                                                     fill_type="solid")
-                # Tampilkan warna suhu 2 pada label
-                self.ui.pilihWarnaSuhu2_btn.setStyleSheet(f"background-color: {fill_suhu2};")
-                self.fill_suhu2 = PatternFill(start_color=fill_suhu2.lstrip('#'),
-                                                     end_color=fill_suhu2.lstrip('#'),
-                                                     fill_type="solid")
-                # Tampilkan warna holding time pada label
-                self.ui.pilihWarnaHoldingTime_btn.setStyleSheet(f"background-color: {fill_holding_time};")
-                self.fill_holding_time = PatternFill(start_color=fill_holding_time.lstrip('#'),
-                                                     end_color=fill_holding_time.lstrip('#'),
-                                                     fill_type="solid")
-            if 'Pengaturan Holding Time' in config:
-                holding_time = config.get('Pengaturan Holding Time', 'HoldingTime', fallback='9:00')
-                self.ui.holdingTime_lineEdit.setText(holding_time)
-        except Exception as e:
-            print(f'Error membaca file pengaturan: {e}')
-
     # Metode dari Utilities/windows_func.py
     browse_files = browse_files
     validate_and_start_import = validate_and_start_import
@@ -166,6 +97,8 @@ class MainWindow(QtWidgets.QMainWindow):
     fileExists = fileExists
     
     # Metode dari Utilities/pengaturan_func.py
+    simpan_pengaturan_conf = simpan_pengaturan_conf
+    baca_pengaturan_conf = baca_pengaturan_conf
     get_batas_suhu_1 = get_batas_suhu_1
     get_batas_suhu_2 = get_batas_suhu_2
     pilih_warna_suhu1 = pilih_warna_suhu1
